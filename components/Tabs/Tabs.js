cab9b7fe439965ef5ad4e5ad1740daa5d59a4b57
vebradev <justinas@vebra.dev>
2019-05-16 16:10:44 +0300

    Add Tablink class with select() method.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,33 +2,30 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
-    // this.data;
-    
+    this.data = element.dataset.tab;
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
-    
+    this.tabItem = new TabItem(this.itemElement);  
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener("click", () => this.select());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll(".tabs-link");
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
-
+    Array.from(links).forEach(el => {
+      el.classList.remove("tabs-link-selected");
+      console.log(this.links);
+    });
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add("tabs-link-selected")
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select(); 
   }
 }
 
@@ -59,4 +56,8 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+let links = document.querySelectorAll('.tabs-link');
+
+links.forEach(element => {
+  return new TabLink(element);
+});
